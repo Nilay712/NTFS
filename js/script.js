fed42838f0a241afe9736cf6d727beadca1040af
Nilay712 <ghoshnilaykumar@gmail.com>
2025-08-16 07:53:09 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+
+
+// $(document).ready(function () {
+//     $('.owl-carousel').owlCarousel({
+//         stagePadding: 120,
+//         loop: true,
+//         margin: 30,
+//         nav: true,
+//         dots: false,
+//         responsive: {
+//             576: {
+//                 items: 2
+//             },
+//             768: {
+//                 items: 3
+//             },
+//             1200: {
+//                 items: 3
+//             }
+//         }
+//     })
+
+// });
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 62,
+        },
+    },
+});
